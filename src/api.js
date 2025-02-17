71c23d331e2036ace7e3111ac803737da47a5e62
Ludens509 <alexandreludens2@gmail.com>
2025-02-17 12:11:04 -0500

    17 FEB 12:10

--- src/api.js
+++ src/api.js
@@ -1,24 +1,23 @@
 export const fetchHolidays = async (apiKey, country, year, month, day) => {
-  const baseUrl = "http://localhost:5000/api/holidays"; // Proxy server URL
-  // const baseUrl = "https://holidayz-api-app.onrender.com/api/holidays"; // Proxy server URL on RENDER
+  // const baseUrl = "http://localhost:5000/api/holidays"; // Proxy server URL
+  const baseUrl = "https://holidayz-api-app.onrender.com/api/holidays"; // Proxy server URL on RENDER
   let url = `${baseUrl}?key=${apiKey}&country=${country}&year=${year}`;
 
   if (month) url += `&month=${month}`;
   if (day) url += `&day=${day}`;
 
-  try {
-    const response = await fetch(url);
 
-    // Check if the response is OK (status code 200-299)
-    if (!response.ok) {
-      // Read the response body only once
-      throw response;
-    }
-    const data = await response.json()
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error("Fetch error:", error);
-    throw error;
+  const response = await fetch(url);
+
+  // Check if the response is OK (status code 200-299)
+  if (!response.ok) {
+    // Read the response body only once
+    throw response;
   }
+  const data = await response.json()
+  console.log(data);
+  return data;
+
+
+
 };
